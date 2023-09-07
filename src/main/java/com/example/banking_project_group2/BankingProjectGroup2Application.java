e2d1f4a391618a5e4c648066404114b33420bf5c
unknown <singhbhavini18@gmail.comgit config --global user.name Bhavini20>
2023-09-07 12:02:33 +0530

    adding structure files

---
+++ src/main/java/com/example/banking_project_group2/BankingProjectGroup2Application.java
@@ -0,0 +1,13 @@
+package com.example.banking_project_group2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingProjectGroup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingProjectGroup2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/banking_project_group2/BankingProjectGroup2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banking_project_group2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingProjectGroup2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
