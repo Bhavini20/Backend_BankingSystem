2200652197e8499b566424f13f21cea0816482ca
unknown <singhbhavini18@gmail.comgit config --global user.name Bhavini20>
2023-09-09 15:11:30 +0530

    transaction controller

--- src/main/java/com/example/banking_project_group2/model/Customer.java
+++ src/main/java/com/example/banking_project_group2/model/Customer.java
@@ -1,8 +1,18 @@
 package com.example.banking_project_group2.model;
 //package com.example.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 //import jakarta.persistence.Entity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "customer")
@@ -41,6 +51,9 @@ public class Customer {
 	private String password;
 	@Column(name = "age")
 	private int age;
+	
+	@OneToMany(cascade=CascadeType.ALL,mappedBy="cust_id")
+	private List<Account> account = new ArrayList<>();
 	
 	public Customer() {
 		
--- src/main/java/com/example/banking_project_group2/model/Transactions.java
+++ src/main/java/com/example/banking_project_group2/model/Transactions.java
@@ -1,5 +1,94 @@
 package com.example.banking_project_group2.model;
 
-public class Transactions {
+import java.sql.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="transaction")
+public class Transactions{
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int transaction_id;
+	
+	@Column
+	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+	private Date trans_time;
+	
+	@Column
+	private Account from_acc;	
+
+	@Column
+	private Account to_acc;
+	
+	@Column
+	private int amount;
+	
+	
+	
+	public Transactions() {
+		
+	}
+	
+	public Transactions(int transaction_id, Date trans_time, Account from_acc, Account to_acc, int amount) {
+		super();
+		this.transaction_id = transaction_id;
+		this.trans_time = trans_time;
+		this.from_acc = from_acc;
+		this.to_acc = to_acc;
+		this.amount = amount;
+	}
+
+	public int getTransaction_id() {
+		return transaction_id;
+	}
+
+	public void setTransaction_id(int transaction_id) {
+		this.transaction_id = transaction_id;
+	}
+
+	public Date getTrans_time() {
+		return trans_time;
+	}
+
+	public void setTrans_time(Date trans_time) {
+		this.trans_time = trans_time;
+	}
+
+	public Account getFrom_acc() {
+		return from_acc;
+	}
+
+	public void setFrom_acc(Account from_acc) {
+		this.from_acc = from_acc;
+	}
+
+	public Account getTo_acc() {
+		return to_acc;
+	}
+
+	public void setTo_acc(Account to_acc) {
+		this.to_acc = to_acc;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	
+	
+	
 
 }
