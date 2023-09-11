6c247560b371acc334a3771f8f204dc35563a47a
ashueep <ashuanand29@gmail.com>
2023-09-11 16:14:44 +0530

    Added JWT Auth

--- src/main/java/com/example/banking_project_group2/controller/AuthController.java
+++ src/main/java/com/example/banking_project_group2/controller/AuthController.java
@@ -4,7 +4,10 @@ import com.example.banking_project_group2.dto.LoginDTO;
 import com.example.banking_project_group2.dto.RegisterDTO;
 import com.example.banking_project_group2.model.Customer;
 import com.example.banking_project_group2.repository.CustomerRepository;
+import com.example.banking_project_group2.security.JWTGen;
+import com.example.banking_project_group2.dto.AuthResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -24,16 +27,19 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
     private CustomerRepository customerRepository;
     private PasswordEncoder passwordEncoder;
+    private JWTGen jwtgen;
 
     @Autowired
-    public AuthController(AuthenticationManager authenticationManager, CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+    public AuthController(AuthenticationManager authenticationManager, JWTGen jwtgen, CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.customerRepository = customerRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtgen = jwtgen;
     }
 
     @PostMapping("/register")
     public Customer register(@RequestBody RegisterDTO registerDTO){
+    	System.out.println("[/register]");
         if(customerRepository.findByUsername(registerDTO.getUsername()) != null){
             return new Customer(0, "null", "null", 0);
         }
@@ -47,16 +53,19 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO){
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO loginDTO){
         System.out.println("[/login]");
-        Customer customer = customerRepository.findByUsername(loginDTO.getUsername());
-        if(customer == null) return new ResponseEntity<>("No such user found", HttpStatusCode.valueOf(403));
-
-        if(passwordEncoder.matches(loginDTO.getPassword(), customer.getPassword())){
-            return new ResponseEntity<>("User logged in", HttpStatusCode.valueOf(200));
-        }
-
-        return new ResponseEntity<>("Wrong password", HttpStatusCode.valueOf(403));
+        Authentication auth = authenticationManager.authenticate(
+        		new UsernamePasswordAuthenticationToken(
+        				loginDTO.getUsername(),
+        				loginDTO.getPassword()
+        				)
+        		);
+//        System.out.println("";)
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        String token = jwtgen.generateToken(auth);
+        
+        return new ResponseEntity<>(new AuthResponseDTO(token), HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/example/banking_project_group2/security/JWTGen.java
@@ -0,0 +1,49 @@
+package com.example.banking_project_group2.security;
+
+
+import java.security.Key;
+
+//i
+import java.util.Date;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import org.springframework.stereotype.Component;
+import org.springframework.security.core.Authentication;
+import io.jsonwebtoken.security.Keys;
+@Component
+public class JWTGen {
+//	private String key = "---------------secret------------";
+	private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+	public String generateToken(Authentication auth) {
+		String username = auth.getName();
+		Date currentDate = new Date();
+		Date expireDate = new Date(currentDate.getTime() + 70000);
+		
+		String token = Jwts.builder().setSubject(username).setIssuedAt(currentDate).
+				setExpiration(expireDate)
+//				.signWith(SignatureAlgorithm.HS512, key)
+				.signWith(key, SignatureAlgorithm.HS512)
+				.compact();
+	
+		return token;
+	}
+	
+	public String getUsernameFromToken(String token) {
+		Claims claim = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+		return claim.getSubject();
+	}
+	
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+//			throw new AuthenticationCredentialsNotFoundException("JWT was expired")
+			return false;		
+		}
+	}
+	
+}
