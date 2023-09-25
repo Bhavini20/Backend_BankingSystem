58ec3019ebbcb76be9c9aa0563e6075ec3650248
ashueep <ashuanand29@gmail.com>
2023-09-25 10:29:58 +0530

    Removed Token from account service

--- src/main/java/com/example/banking_project_group2/controller/AccountController.java
+++ src/main/java/com/example/banking_project_group2/controller/AccountController.java
@@ -43,12 +43,15 @@ public class AccountController {
 
 	@GetMapping("/viewAccounts")
 	public List<Account> viewAccounts(@Valid @RequestHeader(name = "Authorization") String token) {
-		return accSer.viewAccounts(token);
+		String username = jwtgen.getUsernameFromToken(token.substring(7));
+		return accSer.viewAccounts(username);
 	}
 
 	@GetMapping("/viewAccountNumbers")
 	public List<Integer> viewAccountNumbers(@Valid @RequestHeader(name = "Authorization") String token){
-		List<Account> accounts = accSer.viewAccounts(token);
+
+		String username = jwtgen.getUsernameFromToken(token.substring(7));
+		List<Account> accounts = accSer.viewAccounts(username);
 
 		List<Integer> accountNumbers = new ArrayList<>();
 
